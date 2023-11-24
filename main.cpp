faa7ad2a87e0f7bf715e28518ee4366b7f25b935
Sidnei Jr <xplouses@gmail.com>
2023-11-24 14:23:22 -0300

    trabalhando com array e for

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ using namespace std;
 
 int main() {
 
-    cout << "Digite uma letra: ";
-    
-    char a = _getch();
-    
-    cout << "\n\nletra pressionada foi: " << a;
+    bool gameEnd = false;
 
+    string options[5] = {"Solo", "Multijogador", "Opcoes", "Conquistas", "Sair"};
+
+    while (!gameEnd)
+    {
+        for (size_t i = 0; i < sizeof(options) / sizeof(string); i++)
+        {
+            cout << options[i] << "\n\n";
+        }
+
+        gameEnd = true;
+    }
+    
     return 0;
 }
