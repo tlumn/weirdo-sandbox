f98d15e70cb7f5942853a1887618440e584d10f8
Sidnei Jr <xplouses@gmail.com>
2023-11-24 15:01:04 -0300

    switch funcionando

--- main.cpp
+++ main.cpp
@@ -9,15 +9,39 @@ int main() {
 
     string options[5] = {"Solo", "Multijogador", "Opcoes", "Conquistas", "Sair"};
 
+    //Game Loop
     while (!gameEnd)
     {
+        // Clear
+        system("CLS");
+
+        // Menu Draw
         for (size_t i = 0; i < sizeof(options) / sizeof(string); i++)
         {
-            cout << options[i] << "\n\n";
+            cout << i + 1 << ". " << options[i];
+
+            cout << "\n\n";
         }
 
-        gameEnd = true;
+        // Get User Key
+        char key = _getch();
+        key = tolower(key);
+
+        // Process User Key
+        if (key == 's' || key == 'w') 
+        {
+            switch (key)
+            {
+            case 's':
+                break;
+
+            case 'w':
+                break;
+            }
+
+        } else if (key == '5')
+        {
+            gameEnd = true;
+        }
     }
-    
-    return 0;
 }
