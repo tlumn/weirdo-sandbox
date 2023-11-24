6bf6a795d5cdaa267bb20ac999fa8b77808a0e01
Sidnei Jr <xplouses@gmail.com>
2023-11-24 13:39:52 -0300

    aprendendo a detectar caracteres

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+    cout << "Digite uma letra: ";
+    
+    char a = _getch();
+    
+    cout << "\n\nletra pressionada foi: " << a;
+
+    return 0;
+}
