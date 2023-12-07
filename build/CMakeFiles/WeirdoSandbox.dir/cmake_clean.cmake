d38deab1e5224232ddd2e5c803fbef04ff13f6ac
Sidnei Jr <xplouses@gmail.com>
2023-12-06 22:32:41 -0300

    executavel funcionando na bin

---
+++ build/CMakeFiles/WeirdoSandbox.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/fazer-backup/projetos/weirdo-sandbox/bin/WeirdoSandbox.exe"
+  "C:/fazer-backup/projetos/weirdo-sandbox/bin/WeirdoSandbox.exe.manifest"
+  "C:/fazer-backup/projetos/weirdo-sandbox/bin/WeirdoSandbox.pdb"
+  "C:/fazer-backup/projetos/weirdo-sandbox/bin/libWeirdoSandbox.dll.a"
+  "CMakeFiles/WeirdoSandbox.dir/C_/fazer-backup/projetos/weirdo-sandbox/src/core/main.cpp.obj"
+  "CMakeFiles/WeirdoSandbox.dir/C_/fazer-backup/projetos/weirdo-sandbox/src/core/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WeirdoSandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
