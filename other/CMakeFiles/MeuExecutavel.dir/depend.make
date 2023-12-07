5789bc47ad187f4e5f6b933d00d6f2ead3447eba
Sidnei Jr <xplouses@gmail.com>
2023-12-06 21:55:48 -0300

    funcionando cmake

---
+++ other/CMakeFiles/MeuExecutavel.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MeuExecutavel.
+# This may be replaced when dependencies are built.
---
+++ other/CMakeFiles/WeirdoSandbox.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WeirdoSandbox.dir/C_/fazer-backup/projetos/weirdo-sandbox/project/main.cpp.obj"
+  "CMakeFiles/WeirdoSandbox.dir/C_/fazer-backup/projetos/weirdo-sandbox/project/main.cpp.obj.d"
+  "WeirdoSandbox.exe"
+  "WeirdoSandbox.exe.manifest"
+  "WeirdoSandbox.pdb"
+  "libWeirdoSandbox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WeirdoSandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
